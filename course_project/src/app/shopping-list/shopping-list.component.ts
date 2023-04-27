1bbf83a2f1cef0a5075bbad637114ee03897f5a5
Jordan1211 <gre20057@byui.edu>
2023-04-26 23:40:16 -0600

    added all files

---
+++ course_project/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { ingredient } from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent {
+  ingredients: ingredient[] = [
+    new ingredient('Apples', 5),
+    new ingredient('Tomatoes', 10),
+
+  ];
+
+  constructor() {}
+
+}
