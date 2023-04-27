df10ef60dfbea9a54864d7c6c738da52a4e0c786
Jordan1211 <gre20057@byui.edu>
2023-04-27 01:16:14 -0600

    first try at assignment

---
+++ week2/cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Contact } from './contact.model';
+
+@Component({
+  selector: 'cms-contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.css']
+})
+export class ContactListComponent {
+  contacts: Contact[] = [
+    new Contact( 1, 'Lola Gershum', 'lgw@gmail.com', '801-123-5589' , '../../assets/images/barzeer.jpg', []),
+    new Contact( 2, 'June Johanson', 'jjo@gmail.com', '801-123-7849' , '../../assets/images/jacksonk.jpg', [])  
+  ]
+}
