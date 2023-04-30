8439b2d776733402fe43300be1b1d6549b61efac
Jordan1211 <gre20057@byui.edu>
2023-04-29 18:01:47 -0600

    cms project changes

--- cms/src/app/contacts/contact-detail/contact-detail.component.ts
+++ cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { Contact } from '../contact-list/contact.model';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-detail',
   templateUrl: './contact-detail.component.html',
   styleUrls: ['./contact-detail.component.css']
 })
+
 export class ContactDetailComponent {
   contact= 
     new Contact( 1, 'Lola Gershum', 'lgw@gmail.com', '801-123-5589' , '../../assets/images/barzeer.jpg', [])
--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Contact } from './contact.model';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-list',
@@ -10,5 +10,12 @@ export class ContactListComponent {
   contacts: Contact[] = [
     new Contact( 1, 'Lola Gershum', 'lgw@gmail.com', '801-123-5589' , '../../assets/images/barzeer.jpg', []),
     new Contact( 2, 'June Johanson', 'jjo@gmail.com', '801-123-7849' , '../../assets/images/jacksonk.jpg', [])  
-  ]
+  ];
+
+  construction() {}
+
+  ngOnInit() {
+    
+  }
+
 }
