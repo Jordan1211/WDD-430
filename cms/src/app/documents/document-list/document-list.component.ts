a5e69aea8dc61b85021b7b971d40a171ed46dd99
Jordan1211 <90577914+Jordan1211@users.noreply.github.com>
2023-05-08 22:13:27 -0600

    updates for week 4

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  documents: Document[] = [
+  new Document( 1, 'MARVEL - Rocket Racoon', 'This is a test document. Just needs to be a little bit longer to match the assignment.' , 'www.google.com', []),
+  new Document( 2, 'MARVEL - Star Lord', 'This is a test document. Just needs to be a little bit longer to match the assignment.' , 'www.marvel.com', []),
+  new Document( 3, 'MARVEL - Peter Parker', 'This is a test document. Just needs to be a little bit longer to match the assignment.' , 'www.twitter.com', []),  
+  new Document( 4, 'MARVEL - Miles Morales', 'Yet another test document. Just needs to be a little bit longer to match the assignment.' , 'www.disney.com', []),
+  new Document( 5, 'MARVEL - Wand Vision', 'Another one (DJ Khaled Voice). Just needs to be a little bit longer to match the assignment.' , 'www.facebook.com', [])    
+ ];
+
+onSelected(document: Document) {
+  this.selectedDocumentEvent.emit(document);
+}
+construction() {}
+
+ngOnInit() {
+}
 
 }
